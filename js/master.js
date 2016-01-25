fbe633bf6914c6a0b76d93ed59ecba6a725388ec
Isabelle George and Tim Durward <timothy.a.durward@gmail.com>
2016-01-25 10:43:06 -0800

    add for-in loop for task objects

--- js/master.js
+++ js/master.js
@@ -6,7 +6,17 @@ function TodoList(task1, task2, task3){
   this.task3 = task3;
 }
 
+$(document).ready(function() {
+  $('#createList').click(function() {
+    var task1 = $('#firstTask').val();
+    var task2 = $('#secondTask').val();
+    var task3 = $('#thirdTask').val();
+    var newTodoList = new TodoList(task1, task2, task3);
+    $('#firstTask, #secondTask , #thirdTask' ).val("");
+    for(x in newTodoList){ $('.notDone').append("<li><span>" + newTodoList[x] + "</span></li>")}
 
+  });
+});
 
 
 //User input Logic
