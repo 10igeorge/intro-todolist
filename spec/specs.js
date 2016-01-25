0b7606e017505e2bc26e26fcb97965fc4c919835
Isabelle George and Tim Durward <timothy.a.durward@gmail.com>
2016-01-25 09:37:49 -0800

    add first spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,12 @@
+describe('TodoList', function() {
+  it("will create a task list based on user input", function() {
+    var newTodoList = new TodoList("Buy groceries", "Go to the doctor", "Shower");
+    expect(newTodoList.task1).to.equal("Buy groceries");
+    expect(newTodoList.task2).to.equal("Go to the doctor");
+    expect(newTodoList.task3).to.equal("Shower");
+  });
+});
+
 
 // Example Spec below:
 
