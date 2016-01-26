db8539f8cca1d33c2db5e729fb87fa4ea7fc5c24
Isabelle George and Tim Durward <timothy.a.durward@gmail.com>
2016-01-25 16:20:57 -0800

    fix typos

--- js/master.js
+++ js/master.js
@@ -15,17 +15,18 @@ $(document).ready(function() {
     var task2 = $('#secondTask').val();
     var task3 = $('#thirdTask').val();
     var newTodoList = new TodoList(task1, task2, task3);
-    $('.toBeDone, .completedTask').show();
+    $('.container1').show();
     $('#firstTask, #secondTask , #thirdTask' ).val("");
     for(x in newTodoList){
-       $('.notDone').append("<li><input type='checkbox' id='" + newTodoList[x] + "'><label for='" + newTodoList[x] + "'> &emsp;" + newTodoList[x] + "</label></li>" );
+       $('.notDone').append("<li><input type='checkbox' id='" + newTodoList[x] + "'><label for='" + newTodoList[x] + "'> &emsp;" + newTodoList[x] + "</label></li>");
      }
-  $('#moveToCompleted').click(function() {
-    var inputCheck = $("input").is(":checked");
-    if(inputCheck === true){
-      var checkedLI = $(":checked").parent().detach();
-      $("#completeTaskList").append(checkedLI);
-    }
-  });
+    $('#moveToCompleted').click(function() {
+      var inputCheck = $("input").is(":checked");
+      if(inputCheck === true){
+        var checkedLI = $(":checked").parent().detach();
+        $("#completeTaskList").append(checkedLI);
+      }
+    });
+
   });
 });
